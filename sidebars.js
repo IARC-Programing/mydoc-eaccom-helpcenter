e9ee2fe0290a82e689afdc14bf15eebdf6affbd8
theethawat <5910110150@psu.ac.th>
2022-12-20 16:14:12 +0700

    เพิ่มรายการการจองห้องประชุม

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,10 @@
 module.exports = {
   someSidebar: {
     เริ่มต้น: ['intro'],
-    ห้องประชุม: ['meetingroom/create-edit-meeting-room'],
+    ห้องประชุม: [
+      'meetingroom/create-edit-meeting-room',
+      'meetingroom/create-meeting-order',
+    ],
 
     นโยบาย: ['policy', 'condition', 'cookies', 'contact'],
   },
